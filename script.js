1b1316297b9725d6a1bb4ad5e6964c3e7e75769f
Ben Hopkins <bjhops17@gmail.com>
2020-12-16 08:09:48 -0800

    initial commit

---
+++ script.js
@@ -0,0 +1,12 @@
+var currentDate = document.querySelector("#currentDay")
+currentDate = new Date()
+console.log(currentDate)
+
+var todo = JSON.parse(localStorage.getItem("input")) || []
+localStorage.setItem("input", JSON.stringify(todo))
+todo.push
+var input = document.querySelector("col-8")
+
+getitem = (key)
+
+todo//new variable
